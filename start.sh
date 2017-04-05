f4cd6b9667b4649b4ef8b4bb645850b05aceebde
Samuli Piippo <samuli.piippo@qt.io>
2017-04-05 11:35:58 +0300

    Update start.sh script
    config.yaml from AM no longer needed
    
    Change-Id: I1fe4a47f1ae44681daad24dcd2725534224e2351
    Reviewed-by: Katja Marttila <katja.marttila@qt.io>
    Reviewed-by: Samuli Piippo <samuli.piippo@qt.io>

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 export QT_QPA_EGLFS_KMS_CONFIG=kms_config.json
 cd /opt/automotivedemo
-/usr/bin/appcontroller /usr/bin/appman -r -c /opt/am/config.yaml -c am-config.yaml --dbus none Main.qml
+/usr/bin/appcontroller /usr/bin/appman -r -c am-config.yaml --dbus none
 
